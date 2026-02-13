accc875eb041f27c682d95accf5ac40f957ac33d
sadvakasovdamir90-lab <sadvakasov.damir90@gmail.com>
2026-02-13 21:19:57 +0500

    Update script.js

--- script.js
+++ script.js
@@ -51,9 +51,6 @@ async function handlePayment() {
     } catch (e) {
         alert("Ошибка сети!");
     }
-
-    // handlePayment функциясының ішіндегі msg айнымалысын осылай өзгерт:
-const msg = `🎰 ЖАҢА ҚАТЫСУШЫ / НОВЫЙ УЧАСТНИК\n👤 Аты/Имя: ${name}\n📞 Тел: ${phone}\n🎫 Нөмірлер/Номера: ${nums}\n💰 Сумма: ${selected.size * PRICE} ₸`;
-
 }
+
 
